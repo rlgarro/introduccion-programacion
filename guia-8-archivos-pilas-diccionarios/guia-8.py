86a81b22a7545b66ebf942858daa0918207a97d8
Roman Lopez <garrodev18@gmail.com>
2024-11-03 19:30:10 -0300

    ejercicios varios

--- guia-8-archivos-pilas-diccionarios/guia-8.py
+++ guia-8-archivos-pilas-diccionarios/guia-8.py
@@ -1,7 +1,35 @@
+# IMPORTS
+from queue import LifoQueue as Pila
+import random
+
+def imprimir_pila(pila : Pila):
+    if pila.empty():
+        return
+
+    pila_temp = Pila()
+    copiar_pila(pila, pila_temp)
+    print("[", end="")
+    while not (pila.empty()):
+        elem = pila.get()
+        print('{0}, '.format(elem), end="")
+
+    print("]")
+    copiar_pila(pila_temp, pila)
+
+# Genera una nueva pila con los mismos elemntos de la ingresada sin vaciarla.
+def copiar_pila(pila : Pila, pilaCopy : Pila) -> Pila:
+    pila_temp = Pila()
+    while not (pila.empty()):
+        elem = pila.get()
+        pilaCopy.put(elem)
+        pila_temp.put(elem)
+
+    while not (pila_temp.empty()):
+        pila.put(pila_temp.get())
+
+
 '''
-#######################
-##### EJERCICIO I #####
-#######################
+##### EJERCICIO 1
 
 Enunciado: 
 Implementar una funcion generar_nros_al_azar(in cantidad : int, in desde : int, in hasta: int) -> Pila[int] que genere una pila de cantidad de numeros enteros al azar en el rango [desde, hasta]
@@ -15,8 +43,6 @@ Pueden usar la funcion random.randint(< desde >, < hasta >) y la clase LifoQueue
     p.empty() # vacia?
 
 '''
-from queue import LifoQueue as Pila
-import random
 
 def generar_nros_al_azar(cantidad : int, desde : int, hasta : int) -> Pila:
     pila = Pila()
@@ -25,33 +51,77 @@ def generar_nros_al_azar(cantidad : int, desde : int, hasta : int) -> Pila:
 
     return pila
 
+'''
+#### EJERCICIO 2
 
-def imprimir_pila(pila : Pila):
-    if pila.empty():
-        return
+Enunciado: 
+Implementar una funcion cantidad_elementos(in p : Pila) -> int que, dada una pila, cuente y devuelva la cantidad de elementos que contiene. No se puede utilizar la funcion LifoQueue.qsize(). Si se usa get() para recorrer la pila, esto modifica el parametro de entrada. Y como la especificacion dice que es de tipo in hay que restaurarla.
+'''
 
-    pila_temp = Pila()
-    print("[", end="")
-    while not (pila.empty()):
+def cantidad_elementos(pila : Pila) -> int:
+    if (pila.empty()):
+        return 0
+
+    cantidad = 0
+    pila_temporal : Pila = Pila()
+    copiar_pila(pila, pila_temporal)
+    while not(pila.empty()):
+        pila.get()
+        cantidad += 1
+
+
+    copiar_pila(pila_temporal, pila)
+    return cantidad
+
+'''
+#### EJERCICIO 3
+'''
+
+def buscar_el_maximo(pila : Pila[int]) -> int:
+    if (pila.empty()):
+        return 0
+
+    maximo : int = 0
+    pila_temporal : Pila = Pila()
+    copiar_pila(pila, pila_temporal)
+
+    while not(pila.empty()):
         elem = pila.get()
-        print('{0}, '.format(elem), end="")
-        pila_temp.put(elem)
+        if elem > maximo:
+            maximo = elem
 
-    print("]", end="")
-    poner_todos(pila_temp, pila)
+    copiar_pila(pila_temporal, pila)
+    return maximo
 
-# pone todos los elem de pila en pila2
-def poner_todos(pila : Pila, pila2 : Pila) -> Pila:
-    while not (pila.empty()):
-        pila2.put(pila.get())
+'''
+#### EJERCICIO 4
+'''
 
+def buscar_nota_maxima(pila : Pila[tuple[str,int]]) -> tuple[str, int]:
+    pila_temporal = Pila()
+    copiar_pila(pila, pila_temporal)
 
-def get_unmodified(pila : Pila):
-    last = pila.get()
-    pila.put(last)
-    return last
+    maximo : tuple[str, int] = ("", 0)
+    while not(pila.empty()):
+        nombre, nota = pila.get()
+        if nota > maximo[1]:
+            maximo = (nombre, nota)
 
-imprimir_pila(generar_nros_al_azar(100,1,100))
-# usando la funcion dos veces probamos que pilita1 quedo desafectada
-# imprimir_pila(pilita1)
+    copiar_pila(pila_temporal, pila)
+    return maximo
 
+def esta_bien_balanceada(formula : str) -> bool:
+    if formula == "":
+        return True
+
+    parentesis_a_cerrar : int = 0
+    for character in formula:
+        if character == '(':
+            parentesis_a_cerrar += 1
+        elif character == ')' and parentesis_a_cerrar > 0:
+            parentesis_a_cerrar -= 1
+
+    if parentesis_a_cerrar > 0:
+        return False
+
+    return True
